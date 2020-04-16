7865b812a684291ad5f4810f88be8b9bd73cd2fa
Waitti <waitti@foxmail.com>
2020-04-16 16:32:17 +0800

    init

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name="HiCityData",
+    version="1.0.0",
+    description="data tools for HiCity and HiCityServer",
+    packages=find_packages()
+)
