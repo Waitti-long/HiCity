fe2d1214c22345be9c6f1a834f237c63b7cc5036
Waitti <waitti@foxmail.com>
2020-04-27 19:37:00 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="HiCity",
     author="Waitti",
-    version="0.8",
+    version="0.9",
     install_requires=["certifi>=2020.4.5.1", "chardet>=3.0.4", "click>=7.1.1", "Flask>=1.1.2", "idna>=2.9",
                       "itsdangerous>=1.1.0", "Jinja2>=2.11.2", "MarkupSafe>=1.1.1", "requests>=2.23.0",
                       "SQLAlchemy>=1.3.16", "urllib3>=1.25.8", "Werkzeug>=1.0.1", "wincertstore>=0.2",
